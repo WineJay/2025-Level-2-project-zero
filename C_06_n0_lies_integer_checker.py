0c2a34d8b25fde0fce7a4af3af2139c803d0b508
WineJay <hossaina3745@masseyhigh.school.nz>
2025-02-18 09:32:13 +1300

    completed program files

--- C_06_n0_lies_integer_checker.py
+++ C_06_n0_lies_integer_checker.py
@@ -1,14 +1,14 @@
 # Functions goes here
-def int_check(question, num_type, exit_code=None):
+def int_check(question):
     """checks that the user enter an integer. """
 
-    error + "Oops - please enter an integer. "
+    error = "Oops - please enter an integer. "
 
     while True:
-
         try:
             # Return the response if it's an integer
-            int(input(question))
+            response = int(input(question))
+            return response
 
         except ValueError:
             print(error)
@@ -23,4 +23,15 @@ while True:
     # ask user for their name
     name = input("Name: ")  # replace with call to 'not blank' function!
     # ask for their age and check if it's between 12 and 120
-    int_check("Age: ")
+    age = int_check("Age: ")
+
+    # output error message / success message
+    if age < 12:
+        print(f"{name} is too young")
+        continue
+    elif age > 120:
+        print(f"{name} is too old")
+        continue
+
+    else:
+        print(f"{name} brought a ticket")
